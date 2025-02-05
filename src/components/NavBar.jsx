d21d81cc81d57965524375553bd6a234cdf07a1d
Luka Gogashvili <lukagogashvili999@gmail.com>
2025-02-05 19:32:26 +0400

    Changed name of the h1

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { BsMoon } from "react-icons/bs"
 const NavBar = ({toggleDarkMode}) => {
   return (
   <nav className="flex justify-between items-center py-4 px-4 lg:px-24 shadow-md bg-white dark:bg-[#2b3945]">
-        <h1 className="lg:text-2xl font-bold">Where in the world?</h1>
+        <h1 className="lg:text-2xl font-bold">World Countries</h1>
         <button onClick={toggleDarkMode} className="flex items-center gap-2 text-sm font-medium cursor-pointer"><BsMoon  className="font-bold"/> Dark Mode</button>
     </nav>
 
